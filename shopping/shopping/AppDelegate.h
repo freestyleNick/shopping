2f6496d214df01444e9def9f78acb455115a790b
StatkNick <starknick12345@outlook.com>
2018-11-10 11:53:32 +0800

    创建一个项目

---
+++ shopping/shopping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shopping
+//
+//  Created by nike on 2018/11/10.
+//  Copyright © 2018 nike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shopping/shopping/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shopping
+//
+//  Created by nike on 2018/11/10.
+//  Copyright © 2018 nike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
